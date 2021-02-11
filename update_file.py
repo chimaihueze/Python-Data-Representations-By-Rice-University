a34522090eb1fed4e534af1dc027e3547589d38d
Chima Ihueze <kihueze@gmail.com>
2021-02-11 07:16:22 +0100

    initial commit

--- update_file.py
+++ update_file.py
@@ -1,6 +1,16 @@
 def update_file(input_file, output_file):
+    
+    # Open the target file
     with open(input_file) as f:
+
         for line in f:
-            print(line)
+            prefix = "<pre class='cm'>"
+            if prefix in line and 'print' in line:
+                if line.index(prefix) < line.index('print'):
+                    x = line[line.index(prefix):line.index('</pre>')]
+                    # x = x[x.find("<")]
+                    print(x)
+                    
             
 update_file("docs.html", "out_file.txt")
+
